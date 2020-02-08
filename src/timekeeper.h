f8e16731323ec11d4669bc176181f0f82b180ba2
patrickgue <me@patrickguenthard.ch>
2020-02-08 10:24:19 +0100

    as this is now on GitHub I now license it under the WTFPL

--- src/timekeeper.h
+++ src/timekeeper.h
@@ -1,3 +1,9 @@
+/*
+Copyright © 2020 Patrick Günthard <me@patrickguenthard.ch>
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the COPYING file for more details.
+*/
 #ifndef TIMEKEEPER_H
 #define TIMEKEEPER_H
 
